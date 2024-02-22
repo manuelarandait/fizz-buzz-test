f907c43fdeff4072f2b948153dce34e1f77280bf
manuelarandait <157364247+manuelarandait@users.noreply.github.com>
2024-02-22 23:06:42 +0100

    first commit

---
+++ src/Service/CreateFizzBuzzService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+class CreateBizzBuzzService
+{
+
+    public function __invoke()
+    {
+
+    }
+}
---
+++ tests/Integration/Service/CreateFizzBuzzServiceTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Tests\Integration\Service;
+
+class CreateFizzBuzzServiceTest
+{
+
+}
